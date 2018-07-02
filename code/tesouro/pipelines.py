cbfa4ee485428cc513b168197eafef63f8ff1572
kisters <rafael.kisters@fcamara.com.br>
2018-07-02 10:53:27 -0300

    pipeline to logg info

--- code/tesouro/pipelines.py
+++ code/tesouro/pipelines.py
@@ -4,8 +4,18 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-
+import requests, json, logging
 
 class TesouroPipeline(object):
-    def process_item(self, item, spider):
-        return item
+
+
+	def open_spider(self, spider):
+		self.lines = []
+		logging.log(logging.WARNING, "OPEN SET")
+
+	def close_spider(self, spider):
+		logging.log(logging.WARNING, "CLOSE SET {}".format(len(self.lines)))
+
+	def process_item(self, item, spider):
+		self.lines.append(item)
+		return item
